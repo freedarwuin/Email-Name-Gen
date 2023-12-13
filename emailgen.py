b5c17288cb6a7236e62b17fe1a91ec6092e16d7e
Catnip and Milk <110143188+CatnipandMilk@users.noreply.github.com>
2023-12-13 17:03:14 -0600

    Update emailgen.py

--- emailgen.py
+++ emailgen.py
@@ -32,25 +32,25 @@ def main():
         with open('names.txt', 'r') as names_file:
             names = [name.strip() for name in names_file.readlines()]
     except FileNotFoundError as e:
-        print(f"Error: {e}. Make sure 'prefix.txt' and 'names.txt' files exist.")
+        print(f"Error: {e}. Asegúrese de que existan los archivos 'prefix.txt' y 'names.txt'.")
         return
 
-    num_usernames = input("How many usernames would you like to generate? ")
+    num_usernames = input("¿Cuántos nombres de usuario te gustaría generar? ")
 
     while not num_usernames.isdigit() or int(num_usernames) <= 0:
-        print("Please enter a positive integer.")
-        num_usernames = input("How many usernames would you like to generate? ")
+        print("Introduzca un número entero positivo.")
+        num_usernames = input("¿Cuántos nombres de usuario te gustaría generar? ")
 
     num_usernames = int(num_usernames)
 
-    email_domain = input("Wildcard eMail domain: (ex: @domain.com) ")
+    email_domain = input("Dominio de correo electrónico comodín: (p. ej.: @dominio.com) ")
 
     with open('emails.txt', 'w') as output_file:  # Change the file name to 'emails.txt'
         for _ in range(num_usernames):
             username = generate_username(prefixes, names, email_domain)
             output_file.write(username + '\n')
 
-    print(f"{num_usernames} emails generated and saved to emails.txt")
+    print(f"{num_usernames} Correos electrónicos generados y guardados en emails.txt")
 
 if __name__ == "__main__":
     main()
